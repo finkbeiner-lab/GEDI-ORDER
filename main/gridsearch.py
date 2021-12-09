36c65453fa6e55582dfe8715bbae46220b0b3353
Josh Lamstein <jdlamstein@gmail.com>
2021-12-08 17:10:04 -0800

    added retrain flag to gridsearch

--- main/gridsearch.py
+++ main/gridsearch.py
@@ -4,7 +4,7 @@ import argparse
 import pyfiglet
 
 
-def gridsearch(datadir):
+def gridsearch(datadir, retrain_bool):
     param_dict = {'epochs': 200, 'augmentbool': True, 'batch_size': 64, 'min_contrast': 1, 'max_contrast': 1.3,
                   'random_brightness': .2, 'target_size': (224, 224, 3), 'orig_size': (300, 300, 1),
                   'class_weights': {0: 1., 1: 1.}, 'momentum': .9, 'randomcrop': True}
@@ -19,7 +19,10 @@ def gridsearch(datadir):
                 param_dict['learning_rate'] = lr
                 param_dict['optimizer'] = optimizer
                 Tr = Train(parent_dir=datadir, param_dict=param_dict, preprocess_tfrecs=False, use_neptune=True)
-                Tr.train()
+                if retrain_bool:
+                    Tr.retrain()
+                else:
+                    Tr.train()
 
 
 if __name__ == '__main__':
@@ -29,6 +32,9 @@ if __name__ == '__main__':
                         default='/mnt/finkbeinernas/robodata/Josh/GEDI-ORDER',
                         help='data parent directory',
                         dest='datadir')
+    parser.add_argument('--retrain', type=int, action="store", default=False,
+                        help='Save run info to neptune ai',
+                        dest="retrain")
     args = parser.parse_args()
     print('ARGS:\n', args)
-    gridsearch(args.datadir)
+    gridsearch(args.datadir, args.retrain)
