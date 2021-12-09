80e8f71987d45608441687fc4827e3bcafac5538
Josh Lamstein <jdlamstein@gmail.com>
2021-12-09 12:27:32 -0800

    epochs

--- main/gridsearch.py
+++ main/gridsearch.py
@@ -5,7 +5,7 @@ import pyfiglet
 
 
 def gridsearch(datadir, retrain_bool):
-    param_dict = {'epochs': 1, 'augmentbool': True, 'batch_size': 64, 'min_contrast': 1, 'max_contrast': 1.3,
+    param_dict = {'epochs': 200, 'augmentbool': True, 'batch_size': 64, 'min_contrast': 1, 'max_contrast': 1.3,
                   'random_brightness': .2, 'target_size': (224, 224, 3), 'orig_size': (300, 300, 1),
                   'class_weights': {0: 1., 1: 1.}, 'momentum': .9, 'randomcrop': True, 'histogram_eq': True,
                   'weight_decay': 1e-5}
